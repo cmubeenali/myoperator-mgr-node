48d78903a600c3e14eccecca3ecd8df088db0a48
Azrapay <iftay@yahoo.com>
2021-12-29 12:36:33 +0530

    changes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var router = express.Router();
 server.use('/', router);
 
 router.get('/', function (req, res, next) {
+    console.log("New callback at "+(new Date()).toLocaleString())
     console.log(req.query)
     res.send("SUCCESS");
 });
